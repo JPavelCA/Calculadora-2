dfcbffa7194875296e3639c0e9d4a24bcd57ec38
pavel <pavel@DESKTOP-CH79O77>
2022-09-21 17:34:13 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //agraga el constructor
+            operacion suma1 = new operacion();
+
+            suma1.N11 = int.Parse(textBox1.Text);
+            suma1.N21 = int.Parse(textBox2.Text);
+
+            textBox4.Text = suma1.N11.ToString();
+            textBox5.Text = suma1.N21.ToString();
+            textBox6.Text = suma1.sumar().ToString();
+
+            textBox3.Text = suma1.sumar().ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //agraga el constructor
+            operacion meno1 = new operacion();
+
+            meno1.N11 = int.Parse(textBox1.Text);
+            meno1.N21 = int.Parse(textBox2.Text);
+
+            textBox4.Text = meno1.N11.ToString();
+            textBox5.Text = meno1.N21.ToString();
+            textBox6.Text = meno1. meno().ToString();
+
+            textBox3.Text = meno1.meno().ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //agraga el constructor
+            operacion multi1 = new operacion();
+
+            multi1.N11 = int.Parse(textBox1.Text);
+            multi1.N21 = int.Parse(textBox2.Text);
+
+            textBox4.Text = multi1.N11.ToString();
+            textBox5.Text = multi1.N21.ToString();
+            textBox6.Text = multi1.multi().ToString();
+
+            textBox3.Text = multi1.multi().ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //agraga el constructor
+            operacion divi1 = new operacion();
+
+            divi1.N11 = int.Parse(textBox1.Text);
+            divi1.N21 = int.Parse(textBox2.Text);
+
+            textBox4.Text = divi1.N11.ToString();
+            textBox5.Text = divi1.N21.ToString();
+            textBox6.Text = divi1.dividir().ToString();
+
+            textBox3.Text = divi1.dividir().ToString();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //foreach : enumera los elementos de una colección y
+            //ejecuta su cuerpo para cada elemento de la colección.
+            foreach (Control ctr in this.Controls) //controls clase de la cual vamos a poder crear instancias para diseñar la interfaz de la aplicación
+            {
+                if (ctr is TextBox)
+                    ctr.Text = string.Empty;//Representa la cadena vacía. Este campo es de solo lectura.
+                                            //En el código de la aplicación, este campo se usa más comúnmente en asignaciones para
+                                            //inicializar una variable de cadena en una cadena vacía. Para probar si el valor de
+                                            //una cadena es nullo String.Empty , use el método IsNullOrEmpty .
+            }
+            /* textBox1.Text = "";
+             textBox2.Text = "";
+             textBox4.Text = "";
+             textBox5.Text = "";
+             textBox6.Text = "";
+             textBox3.Text = "";*/
+
+        }
+    }
+}
---
+++ Calculadora/operacion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class operacion
+    {
+        //se ponen en privado los tipos de datos
+        private int N1;
+        private int N2;
+
+        //nos permite proteger o crear límites de acceso a las variables y métodos de una clase,
+        //controlando con ello el comportamiento fuera y dentro de la misma clase
+        public int N11 { get => N1; set => N1 = value; }
+        public int N21 { get => N2; set => N2 = value; }
+
+        //metodo, accion, lo que hace.
+        public int sumar()
+        {
+            return (N1 + N2);
+        }
+         public int meno()
+         {
+             return (N1 - N2);
+         }
+         public int dividir()
+         {
+             return (N1 / N2);
+         }
+         public int multi()
+         {
+             return (N1 * N2);
+         }
+        
+    }
+}
